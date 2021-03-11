68029d1d3399ea3cfdb001a60a5882c60014a996
Saumil Mavani <mavani@usc.edu>
2021-03-10 23:02:40 -0800

    Updated the information

--- src/Components/Experience.js
+++ src/Components/Experience.js
@@ -10,50 +10,28 @@ const Experience = () => {
         <div className="timeline-block timeline-block-right">
           <div className="marker"></div>
           <div className="timeline-content">
-            <h3>2014-2015</h3>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sit omnis laboriosam odio deserunt reprehenderit optio cum ratione sequi pariatur deleniti iste perspiciatis officiis minima iusto hic, ipsa ea dicta velit! Quos, placeat ratione. Sunt cumque delectus asperiores et quas qui.</p>
+            <h3>May 2020 - December 2020</h3>
+            <h4><strong>Marshall School of Business, University of Southern California - Full Stack Developer (Los Angeles)</strong></h4>
+            <p>Worked as a full-stack developer in a team of 4 at Marshall School of Business to create a web application for researchers with published papers. Worked on automating the gathering of data by writing python scripts to eliminate the manual work they have been doing until now. Created an admin interface using Angular, MongoDB, Express, and Node.js to manage the application. Worked on Mapping the authors to their publications automatically as the new papers are added to eliminate the manual hours put into this every month. Followed agile methodology with weekly sprints by concurrently developing and testing the features followed by sprint retrospective to efficiently build the product.</p>
           </div>
         </div>
         {/* - */}
         <div className="timeline-block timeline-block-left">
           <div className="marker"></div>
           <div className="timeline-content">
-            <h3>2014-2015</h3>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sit omnis laboriosam odio deserunt reprehenderit optio cum ratione sequi pariatur deleniti iste perspiciatis officiis minima iusto hic, ipsa ea dicta velit! Quos, placeat ratione. Sunt cumque delectus asperiores et quas qui.</p>
+            <h3>September 2019 - December 2019</h3>
+            <h4><strong>Witsmo - Software Engineer Intern (Los Angeles)</strong></h4>
+            <p>Member of the Mobile Development Team working on React Native and Nodejs. Played crucial roles in designing the backend interface along with requirements gathering of the system. Gathered requirements, defined scope, and scheduled negotiation meetings with the client to define deliverables. Conceptualized multiple wireframes using Balsamiq to help the client focus on functionality early during the design process and reduce technical debt. Managed project and tracked bugs using JIRA to help prioritize upcoming tasks. Enforced agile methodology with 2-week sprints and pair programming.</p>
           </div>
         </div>
         {/* - */}
-        <div className="timeline-block timeline-block-right">
-          <div className="marker"></div>
-          <div className="timeline-content">
-            <h3>2015-2016</h3>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sit omnis laboriosam odio deserunt reprehenderit optio cum ratione sequi pariatur deleniti iste perspiciatis officiis minima iusto hic, ipsa ea dicta velit! Quos, placeat ratione. Sunt cumque delectus asperiores et quas qui.</p>
-          </div>
-        </div>
+
         {/* - */}
-        <div className="timeline-block timeline-block-left">
-          <div className="marker"></div>
-          <div className="timeline-content">
-            <h3>2016-2017</h3>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sit omnis laboriosam odio deserunt reprehenderit optio cum ratione sequi pariatur deleniti iste perspiciatis officiis minima iusto hic, ipsa ea dicta velit! Quos, placeat ratione. Sunt cumque delectus asperiores et quas qui.</p>
-          </div>
-        </div>
+ 
         {/* - */}
-        <div className="timeline-block timeline-block-right">
-          <div className="marker"></div>
-          <div className="timeline-content">
-            <h3>2017-2018</h3>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sit omnis laboriosam odio deserunt reprehenderit optio cum ratione sequi pariatur deleniti iste perspiciatis officiis minima iusto hic, ipsa ea dicta velit! Quos, placeat ratione. Sunt cumque delectus asperiores et quas qui.</p>
-          </div>
-        </div>
+
         {/* - */}
-        <div className="timeline-block timeline-block-left">
-          <div className="marker"></div>
-          <div className="timeline-content">
-            <h3>2018-2020</h3>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sit omnis laboriosam odio deserunt reprehenderit optio cum ratione sequi pariatur deleniti iste perspiciatis officiis minima iusto hic, ipsa ea dicta velit! Quos, placeat ratione. Sunt cumque delectus asperiores et quas qui.</p>
-          </div>
-        </div>
+
       </div>
     </div>
   )
